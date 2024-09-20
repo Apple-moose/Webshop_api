995bf779213b858d4f9ec61ac74560828da1e4f1
Apple-moose <patrissio@gmail.com>
2024-09-20 14:52:29 +0200

    render shit corrections to database_url

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
-# import SQLAlchemy parts
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:secret@localhost:4000/postgres"
+# DATABASE_URL = "postgresql://postgres:secret@localhost:4000/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 # create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
